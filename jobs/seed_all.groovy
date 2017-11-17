854ca3f014b99a97cc0b62c2c8f97da459ad0cbf
Sinha <PSINHA1@jaguarlandrover.com>
2017-11-17 06:29:31 +0000

    test the demo seed

--- jobs/seed_all.groovy
+++ jobs/seed_all.groovy
@@ -1,10 +1,11 @@
-job('Seed All') {
-  steps {
-    dsl {
-      external('jobs/*.groovy')  
-      // default behavior
-      // removeAction('IGNORE')      
-      //removeAction('DELETE')
-    }
+job('Project-JOB-test') {
+  scm {
+    git('https://github.com/PoojaSinha0309/Seed-plugin-devops.git')
   }
+	triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
 }
